5be01d9ca5c95ba21fabe8e9586272e2a27c19a5
Karina <karina.kvych@gmail.com>
2023-12-20 16:55:37 +0000

    Add

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
+const logger = require("./logger");
+const authorize = require("./authorize");
 
+// req => middlewere => res
+app.use([logger, authorize]);
 
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  console.log(user)
+  res.send("Items");
+});
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
